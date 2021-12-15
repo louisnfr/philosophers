46eeaabf0ea72e5d905c4b38bd6141d85593f17e
louisnfr <louis.raffin@gmail.com>
2021-12-15 14:58:11 +0100

    philosophers

--- src/main.c
+++ src/main.c
@@ -6,16 +6,34 @@
 /*   By: lraffin <lraffin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 14:28:10 by lraffin           #+#    #+#             */
-/*   Updated: 2021/12/15 14:31:56 by lraffin          ###   ########.fr       */
+/*   Updated: 2021/12/15 14:57:11 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+void	*start(void)
+{
+	while (1)
+	{
+		printf("eat\n");
+		usleep(1000000);
+		printf("sleep\n");
+		usleep(1000000);
+		printf("think\n");
+		usleep(1000000);
+	}
+}
 
 int	main(int ac, char **av)
 {
 	(void)ac;
 	(void)av;
 
+	pthread_t p;
+	pthread_mutex_t mutex;
+
+	pthread_create(&p, NULL, &start, NULL);
+	pthread_join(p, NULL);
 	return (0);
 }
