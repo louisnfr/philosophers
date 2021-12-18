94c76e94f1399d8e3c282c7e734406ddecbf4f62
Louis Raffin <louis.raffin@gmail.com>
2021-12-19 00:48:16 +0100

    philosophers

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: lraffin <lraffin@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/12/15 14:21:00 by lraffin           #+#    #+#              #
-#    Updated: 2021/12/19 00:11:38 by lraffin          ###   ########.fr        #
+#    Updated: 2021/12/19 00:48:11 by lraffin          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,14 +21,13 @@ SRCS =				\
 		$(UTILS)
 
 INIT =					\
-		init.c			\
-		create_philos.c
+		data.c			\
+		philos.c
 
 PARSING =				\
-		check_input.c
+		input.c
 
 ROUTINE =				\
-		check_end.c		\
 		routine.c		\
 		eat.c
 
--- src/exit/clean_data.c
+++ src/exit/clean_data.c
@@ -6,11 +6,26 @@
 /*   By: lraffin <lraffin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 17:56:30 by lraffin           #+#    #+#             */
-/*   Updated: 2021/12/19 00:45:06 by lraffin          ###   ########.fr       */
+/*   Updated: 2021/12/19 00:46:46 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+static void	is_all_fed(t_data *data)
+{
+	int	i;
+
+	i = -1;
+	while (++i < data->nb_philos)
+	{
+		if (data->philo[i].meal_count < data->must_eat)
+			return ;
+	}
+	pthread_mutex_lock(&data->write.mutex);
+	printf("all the philosophers ate\n");
+	pthread_mutex_unlock(&data->write.mutex);
+}
 
 static void	destroy_mutexes(t_data *data)
 {
